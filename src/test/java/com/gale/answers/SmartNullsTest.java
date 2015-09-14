2a27924740fd993d8376626a32de456d1afc1083
nitu <nitu.saksena@cignex.com>
2015-09-14 11:26:28 +0530

    Update SmartNullsTest.java
    expected exceptions are missing which leads to build failure.

--- src/test/java/com/gale/answers/SmartNullsTest.java
+++ src/test/java/com/gale/answers/SmartNullsTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.exceptions.verification.SmartNullPointerException;
 
 import com.gale.domain.Bar;
 import com.gale.domain.Foo;
@@ -15,14 +16,14 @@ public class SmartNullsTest {
 	
 	@Mock(answer = Answers.RETURNS_SMART_NULLS) Foo mockFoo;
 	
-	@Test
+	@Test(expected=NullPointerException.class)
 	public void nullTest_shouldFail() {
 		Bar b = new Foo().getBar();
 		
 		String result = b.getName(); //throws null pointer
 	}
 	
-	@Test
+	@Test(expected=SmartNullPointerException.class)
 	public void smartNullTest_shouldFail() {
 		Bar b = mockFoo.getBar();
 		
